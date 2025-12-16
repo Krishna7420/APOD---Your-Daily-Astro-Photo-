bcba615f99eb9a8508d6bc33872f94fd76570a73
Shrikrishna Thodsare <123809369+Krishna7420@users.noreply.github.com>
2025-12-16 13:06:30 +0530

    Add files via upload

---
+++ AppConstants.swift
@@ -0,0 +1,13 @@
+//
+//  AppConstants.swift
+//  NASA_APOD
+//
+//  Created by Shrikrishna Thodsare on 15/12/25.
+//
+
+import Foundation
+
+struct AppConstants {
+    static let apiKey = "Demo API Key for testing purposes only"
+    static let baseURL = "https://api.nasa.gov/planetary/apod"
+}
