07fa0bf700c202fd6fe1e0ed370a338538b72c6f
Shrikrishna Thodsare <123809369+Krishna7420@users.noreply.github.com>
2025-12-16 13:44:26 +0530

    Remove unused imports and APOD struct
    Removed unused imports and the APOD struct definition.

--- APOD.swift
+++ APOD.swift
@@ -4,15 +4,3 @@
 //
 //  Created by Shrikrishna Thodsare on 15/12/25.
 //
-
-
-import Foundation
-
-struct APOD: Codable {
-    let title: String
-    let explanation: String
-    let url: String
-    let date: String
-    let media_type: String
-    let hdurl: String?
-}
